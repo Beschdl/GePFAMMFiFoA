50483dce5d9843e76a5051a12b04a4b01d74698b
Beschdl <33841601+Beschdl@users.noreply.github.com>
2020-11-12 15:57:50 +0100

    Pre-Alpha Just some modifications

--- GePFAMMFiFoA.py
+++ GePFAMMFiFoA.py
@@ -27,17 +27,30 @@ def walk():
 
 def sort(list):
 	animated = []
+	frames = []
 	import PIL
 	for img in list:
 		opened = PIL.Image.open(img)
 		width, height = opened.size
 		if width!=height:
 			animated.append(img)
-	return animated
+			frames.append(height/width)
+	return (animated, frames)
+
+def createFile(name, speed, frames):
+	out="{\n	\"animation\": {\n	\"frametime\": "+str(speed)+",\n	\"frames\": [\n"
+	for x in range(frames-1):
+		out+="	"+str(x)+",\n"
+	out+="	"+str(frames-1)+"\n"
+	out+="		]\n	}\n}"
+	print(out)
+
+
 
 def main():
 	importPIL()
 	print(sort(walk()))
-
+	print("\n")
+	createFile(1,1,12)
 
 main()
